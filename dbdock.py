1047aab5de806e81863f67de065f4f51de3d0160
Greg Poisson <gpoisson@rams.colostate.edu>
2017-03-13 20:44:16 -0600

    new svm experiment

--- dbdock.py
+++ dbdock.py
@@ -2,254 +2,156 @@ import numpy as np
 from rdkit import Chem
 from rdkit.Chem import AllChem, Draw, rdMolDescriptors
 from sklearn.svm import SVR
-from sklearn.externals import joblib
 from sklearn import preprocessing
 from sklearn.decomposition import PCA
 import os, sys
-import time
 import matplotlib
 from scipy.spatial import distance
-matplotlib.use('Agg')
 import matplotlib.pyplot as plt
 from sklearn.model_selection import train_test_split
 
-c_val = 1.0
-rebuild_db = False
-db = "lig_db_full_3D.npy"
+name_index = 0
+mol_index = 1
+ki_index = 2
 
-bad_ligs = 0
-sulfur_ligs = 0
-
-if (len(sys.argv) > 2):
-	c_val = int(sys.argv[1])
-	rebuild_db = sys.argv[2]
-
-
-def getDG(name):
-	energies = open("energies_sorted.dat",'r')
-	for line in energies:
-		line = line.split()
-		if (name in line):
-			return float(line[1])
-	return None
-
-def shuffleXY(X,y):
-    p = np.random.permutation(len(y))
-    X_new = []
-    y_new = []
-    for index in p:
-        X_new.append(X[index])
-        y_new.append(y[index])
-    X = np.asarray(X_new)
-    y = np.asarray(y_new)
-    return (X, y)
-
-# Gets Euclidian distances for all HBA/HBD pairs, returns a histogram of their values
-def getDistBins(mol):
-	global bad_ligs
-	hbd = Chem.MolFromSmarts('[#7H,#7H2,#7H3,#8H]')
-	hba = Chem.MolFromSmarts('[#7X1,#7X2,#7X3,#8,#9,#17]')
-	try:
-		mol =  Chem.AddHs(mol)
-		max_dist = 15							# keep track of hba/hbd distances of up to max_dist angstroms
-		AllChem.EmbedMolecule(mol,useRandomCoords=True)
-		r = 1
-		while (r == 1):		# This optimization returns 1 if it did not converge, so it iterates until it converges
-			r = AllChem.UFFOptimizeMolecule(mol)
-		atom_coords = []
-		mol_dists = np.arange(max_dist)
-		bins = np.zeros(max_dist)
-		block = Chem.MolToMolBlock(mol)
-		block = block.split("\n")
-		for line in block:
-			line = line.split()
-			if (len(line) == 16):
-				atom_coords.append([float(line[0]),float(line[1]),float(line[2])])
-		atoms = mol.GetAtoms()
-		hbds = mol.GetSubstructMatches(hbd)
-		hbas = mol.GetSubstructMatches(hba)
-		for d, in hbds:				# go through all hbas and all hbds, compute relative distances
-			for a, in hbas:
-				if a != d:
-					sq_dist = np.sum(np.asarray(atom_coords[a])**2 + np.asarray(atom_coords[d])**2)
-					dist = np.sqrt(sq_dist)
-					bins[int(dist)] += 1
-	except:
-		bad_ligs += 1
-		return [None]
-
-	block_text = (Chem.MolToMolBlock(mol))
-	print block_text
-	
-	return bins
-
-# Extract the features of interest from a specified molecule
-def extractFeatureData(mol):
-	global index_of_1d_features
-	
-	dist_bins = getDistBins(mol)
-
-	smr_vsa = rdMolDescriptors.SMR_VSA_(mol)
-	slogp_vsa = rdMolDescriptors.SlogP_VSA_(mol)
-	peoe_vsa = rdMolDescriptors.PEOE_VSA_(mol)
-	hbd = rdMolDescriptors.CalcNumHBD(mol)
-	hba = rdMolDescriptors.CalcNumHBA(mol)
-	
-	molwt = rdMolDescriptors.CalcExactMolWt(mol)
-	nalrings = rdMolDescriptors.CalcNumAliphaticRings(mol)
-	nalhet = rdMolDescriptors.CalcNumAliphaticHeterocycles(mol)
-	narohet = rdMolDescriptors.CalcNumAromaticHeterocycles(mol)
-	narorings = rdMolDescriptors.CalcNumAromaticRings(mol)
-	nHBA = rdMolDescriptors.CalcNumHBA(mol)
-	nHBD = rdMolDescriptors.CalcNumHBD(mol)
-	rotbounds = rdMolDescriptors.CalcNumRotatableBonds(mol)
-
-	#feats = [smr_vsa,slogp_vsa,peoe_vsa,hbd,hba]
-	#feats = [molwt,nalrings,nalhet,narohet,narorings,nHBA,nHBD,rotbounds]
-	feats = [smr_vsa,slogp_vsa,peoe_vsa,hbd,hba,molwt,nalrings,nalhet,narohet,narorings,nHBA,nHBD,rotbounds,dist_bins]
-	
-	feature_data = []
-	for f in feats:
-		if (isinstance(f,int)):
-			feature_data.append(f)
-		elif (isinstance(f,float)):
-			feature_data.append(f)
-		else:
-			for data in f:
-				feature_data.append(data)
-	feature_data = np.asarray(feature_data)						# convert to numpy 
-	'''
-	print ("smr_vsa:\t{}".format(smr_vsa))
-	print ("slogp_vsa:\t{}".format(slogp_vsa))
-	print ("peoe_vsa:\t{}".format(peoe_vsa))
-	print ("hbd:\t{}".format(hbd))
-	print ("hba:\t{}".format(hba))
-	print ("molwt:\t{}".format(molwt))
-	print ("nalrings:\t{}".format(nalrings))
-	print ("nalhet:\t{}".format(nalhet))
-	print ("narohet:\t{}".format(narohet))
-	print ("narorings:\t{}".format(narorings))
-	print ("nHBA:\t{}".format(nHBA))
-	print ("nHBD:\t{}".format(nHBD))
-	print ("rotbounds:\t{}".format(rotbounds))
-	print ("dist bins:\t{}".format(dist_bins))
-	print ("")
-	#print feature_data
-	'''
-	return feature_data
-
-def readOriginalData(saveNewBin=True):
-	print ("Reading original ligand data")
-	ligs1 = np.load("sorted_ligs_by_deltaG_part_0.npy")
-	ligs2 = np.load("sorted_ligs_by_deltaG_part_1.npy")
-	X = []
-	y = []
-
-	ligs = []
-	print("Extracting feature data")
-	for lig in ligs1:
-		ligs.append(lig)
-		print (lig[0])
-		X.append(extractFeatureData(lig[1]))
-		y.append(lig[2])
-
-	for lig in ligs2:
-		ligs.append(lig)
-		name = (lig[0])
-		print name
-		X.append(extractFeatureData(lig[1]))
-		y.append(lig[2])
-
-	#X, y = shuffleXY(X,y)
-
-	if (saveNewBin):
-		np.save(db,(X,y))
-
-	print("Original ligand data loaded and feature data extracted.")
-	print("Feature data saved into {}".format(db))
-
-	return X, y
-
-def readBinary():
-	ligs = np.load(db)
-	data = ligs[0]
-	X = []
-	for d in data:
-		X.append(np.asarray(d))
-	y = ligs[1]
-
-	#X, y = shuffleXY(X,y)
-
-	return X, y
-
-# normalize
-def normalize(X):
-	mean_features = []
-	std_features = []
-	# compute mean and std of each feature
-	for f in range(len(X[0])):
-		mean_features.append(np.mean(X[:][f]))
-		std_features.append(np.std(X[:][f]))
-	# normalize each ligand feature set
-		X[:][f] -= mean_features[f]
-		X[:][f] /= std_features[f]
-	'''
-	# normalize each ligand feature set
-	for feature in range(len(X)):
-		X[:][f] -= mean_features[f]
-		X[:][f] /= std_features[f]
-	'''
-	return X
+C = 10.0
+epsilon = 0.1
+test_size = 0.05
 
 def main():
-	if (rebuild_db):
-		X, y = readOriginalData(saveNewBin=True)
-		X, y = readBinary()
-
-		X = normalize(X)
-
-		np.save("X.npy",X)
-		np.save("y.npy",y)
-	
-	else:
-		X = np.load("X.npy")
-		y = np.load("y.npy")
-
-	X, y = shuffleXY(X, y)
-
-	naive_x, test_x, naive_y, test_y = train_test_split(X, y, test_size=0.4, random_state=42)
-
-	print ("Checking train size: {}".format(len(naive_x)))
-
-	model = SVR(kernel='rbf',C=c_val)
-	model.fit(naive_x,naive_y)
-
-	train_predictions = model.predict(naive_x)
-	predictions = model.predict(test_x)
-	error = abs(test_y - predictions)
-
-	print ("Mean Error: {}".format(np.mean(error)))
-	print ("Max Error:  {}".format(np.max(error)))
+	#compileFeaturesAndLabels()
+	X = np.load("features.npy")
+	y = np.load("labels.npy")
+	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
+	print("X_train contains {} samples; y_train contains {} labels.".format(len(X_train),len(y_train)))
+	print("X_test contains {} samples; y_test contains {} labels.".format(len(X_test),len(y_test)))
+	clf = SVR(C=C, epsilon=epsilon,kernel='rbf')
+	clf.fit(X_train,y_train)
+	predicted = clf.predict(X_test)
+	errors = abs(predicted - y_test)
+	print(clf.score(X_test,y_test))
 
 	plt.figure()
-	plt.suptitle("Prediction vs Actual Delta G\nTest Set")
-	plt.xlabel("Predicted Delta G")
-	plt.ylabel("Actual Delta G")
-	plt.plot(predictions,test_y,'x',ms=2,mew=3)
 	plt.grid(True)
+	plt.suptitle("SVM - C={} epsilon={}\n  Training Set: {} ligands".format(C,epsilon,len(X_train)))
+	plt.xlabel("Predicted (kcal/mol)")
+	plt.ylabel("Actual (kcal/mol)")
+	plt.plot(predicted,y_test,'x',ms=2,mew=3)
 	plt.show()
 
-	plt.savefig("check_results/oos_{}k_c{}.png".format(len(train_predictions),c_val,))
 
-	plt.figure()
-	plt.suptitle("Prediction vs Actual Delta G\nTraining Set")
-	plt.xlabel("Predicted Delta G")
-	plt.ylabel("Actual Delta G")
-	plt.plot(train_predictions,naive_y,'x',ms=2,mew=3)
-	plt.grid(True)
-	plt.show()
+def compileFeaturesAndLabels():
+	ligs_part_0 = np.load("unsorted_ligs_part_0.npy")
+	ligs_part_1 = np.load("unsorted_ligs_part_1.npy")
 
-	plt.savefig("check_results/is_{}k_c{}.png".format(len(train_predictions),c_val,))
+	ligands = []
+
+	for lig in ligs_part_0:
+		ligands.append(lig)
+
+	for lig in ligs_part_1:
+		ligands.append(lig)
+
+	[features, labels] = getAllFeatures(ligands)
+	np.save("features.npy",features)
+	np.save("labels.npy",labels)
+
+def getAllFeatures(ligands):
+	features = []
+	labels = []
+	for ligand in ligands:
+		if (ligand[mol_index] != None):
+			features.append(computeFeatures(ligand[mol_index]))
+			labels.append(ligand[ki_index])
+	return [np.asarray(features), np.asarray(labels)]
+
+def computeFeatures(mol):
+	numRings = rdMolDescriptors.CalcNumRings(mol)
+	nitrogenCount = countNitrogens(mol)
+	oxygenCount = countOxygens(mol)
+	carbonCount = countCarbons(mol)
+	boronCount = countBorons(mol)
+	phosCount = countPhos(mol)
+	sulfurCount = countSulfurs(mol)
+	fluorCount = countFluorine(mol)
+	iodCount = countIodine(mol)
+	doubleBonds = countDoubleBonds(mol)
+	surf_area = rdMolDescriptors.CalcLabuteASA(mol)
+	mol_weight = rdMolDescriptors.CalcExactMolWt(mol)
+	tpsa = rdMolDescriptors.CalcTPSA(mol)
+	return [numRings, nitrogenCount, oxygenCount, carbonCount, boronCount, phosCount, sulfurCount, fluorCount, doubleBonds, surf_area, mol_weight, tpsa]
+
+def countNitrogens(mol):
+	smile = Chem.MolToSmiles(mol)
+	nitrogenCount = 0
+	for ch in smile:
+		if ch == 'N':
+			nitrogenCount += 1
+	return nitrogenCount
+
+def countOxygens(mol):
+	smile = Chem.MolToSmiles(mol)
+	oxygenCount = 0
+	for ch in smile:
+		if ch == 'O':
+			oxygenCount += 1
+	return oxygenCount
+
+def countCarbons(mol):
+	smile = Chem.MolToSmiles(mol)
+	carbonCount = 0
+	for ch in smile:
+		if ch == 'C':
+			carbonCount += 1
+	return carbonCount
+
+def countBorons(mol):
+	smile = Chem.MolToSmiles(mol)
+	boronCount = 0
+	for ch in smile:
+		if ch == 'B':
+			boronCount += 1
+	return boronCount
+
+def countIodine(mol):
+	smile = Chem.MolToSmiles(mol)
+	iodCount = 0
+	for ch in smile:
+		if ch == 'I':
+			iodCount += 1
+	return iodCount
+
+def countPhos(mol):
+	smile = Chem.MolToSmiles(mol)
+	phosCount = 0
+	for ch in smile:
+		if ch == 'P':
+			phosCount += 1
+	return phosCount
+
+def countFluorine(mol):
+	smile = Chem.MolToSmiles(mol)
+	fluorCount = 0
+	for ch in smile:
+		if ch == 'F':
+			fluorCount += 1
+	return fluorCount
+
+def countSulfurs(mol):
+	smile = Chem.MolToSmiles(mol)
+	sulfurCount = 0
+	for ch in smile:
+		if ch == 'S':
+			sulfurCount += 1
+	return sulfurCount
+
+def countDoubleBonds(mol):
+	smile = Chem.MolToSmiles(mol)
+	doubleBonds = 0
+	for ch in smile:
+		if ch == '=':
+			doubleBonds += 1
+	return doubleBonds
 
 
 main()
---
+++ tfnn2.py
@@ -0,0 +1,54 @@
+# Fit a straight line, of the form y=m*x+b
+
+import tensorflow as tf
+import numpy as np
+
+'''
+Your dataset.
+'''
+#xs = [ 0.00,  1.00,  2.00, 3.00, 4.00, 5.00, 6.00, 7.00] # Features
+#ys = [-0.82, -0.94, -0.12, 0.26, 0.39, 0.64, 1.02, 1.00] # Labels
+
+xs = np.load("features.npy")
+ys = np.load("labels.npy")
+
+'''
+Initial guesses, which will be refined by TensorFlow.
+'''
+m_initial = -0.5 # Initial guesses
+b_initial =  1.0
+
+'''
+Define free variables to be solved.
+'''
+m = tf.Variable(m_initial) # Parameters
+b = tf.Variable(b_initial)
+
+'''
+Define the error between the data and the model as a tensor (distributed computing).
+'''
+ys_model = m*xs+b # Tensorflow knows this is a vector operation
+total_error = tf.reduce_sum((ys-ys_model)**2) # Sum up every item in the vector
+
+'''
+Once cost function is defined, create gradient descent optimizer.
+'''
+optimizer_operation = tf.train.GradientDescentOptimizer(learning_rate=0.001).minimize(total_error) # Does one step
+
+'''
+Something you just have to do.
+'''
+initializer_operation = tf.global_variables_initializer()
+
+'''
+All calculations are done in a session.
+'''
+with tf.Session() as session:
+
+    session.run(initializer_operation)
+
+    _EPOCHS = 10000 # number of "sweeps" across data
+    for iteration in range(_EPOCHS):
+        session.run(optimizer_operation)
+
+    print('Slope:', m.eval(), 'Intercept:', b.eval())
