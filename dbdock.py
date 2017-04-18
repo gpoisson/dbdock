f44ffff1ef3dd313a6cd2322915a4bec8f28a455
Greg Poisson <gpoisson@rams.colostate.edu>
2017-04-18 02:26:25 -0600

    reduced code; dist between H is wip

--- dbdock.py
+++ dbdock.py
@@ -16,20 +16,43 @@ ki_index = 2
 
 C = 10.0
 epsilon = 0.1
-test_size = 0.05
+train_size = 0.005
+test_size = 1 - train_size
+batches = 5
 
 def main():
 	#compileFeaturesAndLabels()
 	X = np.load("features.npy")
 	y = np.load("labels.npy")
-	X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=42)
-	print("X_train contains {} samples; y_train contains {} labels.".format(len(X_train),len(y_train)))
-	print("X_test contains {} samples; y_test contains {} labels.".format(len(X_test),len(y_test)))
-	clf = SVR(C=C, epsilon=epsilon,kernel='rbf')
-	clf.fit(X_train,y_train)
-	predicted = clf.predict(X_test)
-	errors = abs(predicted - y_test)
-	print(clf.score(X_test,y_test))
+
+	models = []
+	X_val = X[0:10000]
+	y_val = y[0:10000]
+	X = X[10001:]
+	y = y[10001:]
+	for batch in range(batches):
+		X_train, X_test, y_train, y_test = train_test_split(X, y, test_size=test_size, random_state=batch)
+		#X_test = X_test[0:4000]
+		#y_test = y_test[0:4000]
+		print("X_train contains {} samples; y_train contains {} labels.".format(len(X_train),len(y_train)))
+		print("X_test contains {} samples; y_test contains {} labels.".format(len(X_test),len(y_test)))
+		clf = SVR(C=C, epsilon=epsilon,kernel='rbf')
+		clf.fit(X_train,y_train)
+		models.append(clf)
+		predicted = clf.predict(X_test)
+		errors = abs(predicted - y_test)
+		print(clf.score(X_test,y_test))
+		print("E: {}".format(np.mean(errors)))
+
+	pred = []
+	mean_pred = []
+	for b in range(batches):
+		predictions = models[b].predict(X_val)
+		pred.append([predictions])
+	for feature in range(len(pred[0])):
+		mean_pred.append(np.mean(predictions[:][feature]))
+	errors = abs(mean_pred - y_val)
+	print("FINAL: {}".format(models[0].score(X_val, y_val)))
 
 	plt.figure()
 	plt.grid(True)
@@ -67,6 +90,7 @@ def getAllFeatures(ligands):
 
 def computeFeatures(mol):
 	numRings = rdMolDescriptors.CalcNumRings(mol)
+	numRotBonds = rdMolDescriptors.CalcNumRotatableBonds(mol)
 	nitrogenCount = countNitrogens(mol)
 	oxygenCount = countOxygens(mol)
 	carbonCount = countCarbons(mol)
@@ -79,7 +103,47 @@ def computeFeatures(mol):
 	surf_area = rdMolDescriptors.CalcLabuteASA(mol)
 	mol_weight = rdMolDescriptors.CalcExactMolWt(mol)
 	tpsa = rdMolDescriptors.CalcTPSA(mol)
-	return [numRings, nitrogenCount, oxygenCount, carbonCount, boronCount, phosCount, sulfurCount, fluorCount, doubleBonds, surf_area, mol_weight, tpsa]
+	dist_hs = recurseMolHCount(mol)
+	return [numRings, nitrogenCount, oxygenCount, carbonCount, boronCount, phosCount, sulfurCount, fluorCount, iodCount, doubleBonds, surf_area, mol_weight, tpsa]
+
+def recurseMolHCount(mol):
+	# Add H atoms to molecule
+	mol = Chem.AddHs(mol)
+	# Build hash table of atoms
+	numAtoms = len(mol.GetAtoms())
+	atom_table = {}
+	for atom_index in range(numAtoms):
+		atom = mol.GetAtomWithIdx(atom_index)
+		atom_table[atom_index] = []
+		neighbors = atom.GetNeighbors()
+		for n in neighbors:
+			atom_table[atom_index].append(n.GetIdx())
+	# Build table of Hs
+	h_table = {}
+	for atom_index in range(numAtoms):
+		if (mol.GetAtomWithIdx(atom_index).GetSymbol() == 'H'):
+			h_table[atom_index] = 'H'
+	# Build distribution of distances betweeen Hs
+	dists = getDists(numAtoms, h_table, atom_table)
+
+def getDists(numAtoms, h_table, atom_table):
+	dists = {}
+	dists[1] = 0
+	dists[2] = 0
+	dists[3] = 0
+	dists[4] = 0
+	dists[5] = 0
+	dists[6] = 0
+	dists[7] = 0
+	dists[8] = 0
+	dists[9] = 0
+	dists[10] = 0
+
+	for h1 in h_table:
+		for h2 in h_table:
+			if (h1 != h2):
+				
+
 
 def countNitrogens(mol):
 	smile = Chem.MolToSmiles(mol)
@@ -154,4 +218,4 @@ def countDoubleBonds(mol):
 	return doubleBonds
 
 
-main()
+#main()
