4a96db42726417d2b5d868b8aa962ade5c11be87
Greg Poisson <gpoisson@rams.colostate.edu>
2017-05-23 09:05:14 -0600

    added skeleton code for funnel procedure

---
+++ funnel.py
@@ -0,0 +1,16 @@
+import sys
+import numpy as np 
+
+def rigidDocking():
+	print("Performing rigid docking...")
+
+def flexibleDocking():
+	print("Performing flexible docking...")
+
+def checkInput():
+	print(len(sys.argv))
+
+if __name__ == "__main__":
+	rigidDocking()
+	flexibleDocking()
+	checkInput()
